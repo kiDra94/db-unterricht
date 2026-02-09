9a1431ba9dc0f4ec5528822a36ad120b81f3aca6
Drazen Plavsic <drazen.p@live.com>
2026-02-09 13:07:22 +0100

    AuthGuard von passport kann als UseGuards verwendet werden

--- apisec/src/app.controller.ts
+++ apisec/src/app.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport'; // unterstützt die Verwendung von Passport-Strategi Guards
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(AuthGuard('basic')) // AuthGuard mit der 'basic' Strategie verwenden
   @Get()
   getHello(): string {
     return this.appService.getHello();
