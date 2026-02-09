31024f8cc2b8c42b0f2f49e381cd161b3ba56399
Drazen Plavsic <drazen.p@live.com>
2026-02-09 13:01:55 +0100

    validate bekommt den username und passport schon fix fertig von PassportStrategy gesplite und ueberprueft

--- apisec/src/basic.strategy.ts
+++ apisec/src/basic.strategy.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/require-await */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable prettier/prettier */
+import { UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport"
 import { BasicStragey as HTTPBasicStrategy } from "passport-http";
 
@@ -7,5 +9,11 @@ import { BasicStragey as HTTPBasicStrategy } from "passport-http";
 export class BasicStrategy extends PassportStrategy(HTTPBasicStrategy) {
     constructor() {
         super();
+    }
+    async validate(username: string, password: string) {
+        if (username ===  'user' && password === 'secret') {
+            return { username: 'user' };
+        }
+        throw new UnauthorizedException('Invalid credentials');
     }
 }
