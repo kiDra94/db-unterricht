7d9d1d831ab15ade83055f6202591b0bfcbc2b4c
Drazen Plavsic <drazen.p@live.com>
2026-03-02 14:01:22 +0100

    imported google strategy to the module as sa provider

--- oauth/withGoogle/google/src/app.module.ts
+++ oauth/withGoogle/google/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config'
+import { GoogleStrategy } from './google.strategy';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true })
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GoogleStrategy],
 })
 export class AppModule {}
