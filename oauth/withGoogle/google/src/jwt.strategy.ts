7109abaa27edc057dd9df397d625980faa4516f3
Drazen Plavsic <drazen.p@live.com>
2026-03-06 12:51:58 +0100

    added logic for jwtFromRequest

--- oauth/withGoogle/google/src/jwt.strategy.ts
+++ oauth/withGoogle/google/src/jwt.strategy.ts
@@ -1,12 +1,17 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-jwt";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { Request } from "express";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(private readonly) {
         super({
             jwtFromRequest: (req: Request) => {
+                let token = ExtractJwt.fromAuthHeaderAsBearerToken();
+                if(!token && req.cookies) {
+                    token = req.cookies.accesToken;
+                }
                 return token;
             },
             secretOrKey: process.env.JWT_SECRET || 'fallback-secret',
