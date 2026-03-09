a08dc655c4ee5e8cdbb1fe54e644c3cb9117d645
Drazen Plavsic <drazen.p@live.com>
2026-03-09 13:38:03 +0100

    implemented cors credentials

--- oauth/withGoogle/google/src/main.ts
+++ oauth/withGoogle/google/src/main.ts
@@ -5,6 +5,10 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true, // Allow cookies to be sent with requests
+  })
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
